03325ac0eb2d73017db849619ec38013bb966876
Soham Sonawane <99652332+Sonawanesoham@users.noreply.github.com>
2024-05-07 15:25:49 +0530

    Add files via upload
    MVC

---
+++ Data/UserDBContext.cs
@@ -0,0 +1,23 @@
+﻿using BMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BMS.Data
+{
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("server=C3ITDB7;database=C3IT0790;user=Mkhairnar;password=Abcd1234%;TrustServerCertificate=True");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> BMSUsers { get; set; }
+
+        public DbSet<Posts> BMSPosts { get; set; }
+    }
+}
